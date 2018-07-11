07d8b56efbb5ebad7ddd2b7b5e7744c1e5fa12f0
Vadim BROVENKO <vbrovenk@e3r3p7.unit.ua>
2018-07-11 11:44:21 +0300

    final

--- print_uni_string.c
+++ print_uni_string.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-#include <stdio.h>
 
 static int		uni_putstr(int *string, t_box *info)
 {
